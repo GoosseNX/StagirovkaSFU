27ecb29b368af15dfebc8f4bb7ba0240f9875f39
Nikita <xaustov_hak0361@mail.ru>
2024-05-26 19:49:21 +0700

    For PR

---
+++ Assets/Scenes/Scripts/ItemParent.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemParent : MonoBehaviour
+{
+    public Transform handAttachmentPoint; // пустышка на руке персонажа
+    private Collider _collider; // колайдер объекта(ключа)
+    private Transform _transform; //трансформ объекта(ключа)
+
+    private void Start()
+    {
+        _collider = GetComponent<Collider>();
+        _transform = GetComponent<Transform>();
+    }
+    public void GrubItem()
+    {
+        Invoke("Grub", 1.0f);
+
+    }
+
+    // Метод который привязывает объект к пустышке которая находиться на руке а также устанавливает позицию и поворот как у пустышки
+    private void Grub()
+    {
+        _transform.SetParent(handAttachmentPoint, true);
+        _transform.position = handAttachmentPoint.position;
+        _transform.rotation = handAttachmentPoint.rotation;
+        _collider.enabled = false;
+    }
+
+}
---
+++ Assets/Scenes/Scripts/KillZone.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KillZone : MonoBehaviour
+{
+    [SerializeField] private GameObject player; // Ссылка на игрока
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) // Проверяем, что в килл зону вошел игрок
+        {
+            Ragdoll ragdoll = player.GetComponent<Ragdoll>();
+            if (ragdoll != null)
+            {
+                ragdoll.Enable(); // запуск метода в скрипте рагдолл
+            }
+
+        }
+    }
+}
